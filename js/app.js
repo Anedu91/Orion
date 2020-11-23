b9ac647d131cdf229fc47d593d6545e40daa4d1d
Angel <diaz.angeledu@gmail.com>
2020-11-23 16:58:08 -0400

    Responsive issues and colors

--- js/app.js
+++ js/app.js
@@ -1,23 +1,25 @@
 // Selector variables
-const barChart = document.querySelector("#barChart").getContext("2d");
 
-const lineChart = document.querySelector("#lineChart").getContext("2d");
-const gradientStroke = lineChart.createLinearGradient(500, 0, 100, 0);
-gradientStroke.addColorStop(0, "#FFD422");
-gradientStroke.addColorStop(1, "#FF7D05");
+const createChart = (name) => {
+  return document.getElementById(`${name}`).getContext("2d");
+};
+const chart1 = createChart("barChart");
+const chart2 = createChart("lineChart");
+const gradient1 = chart2.createLinearGradient(500, 0, 100, 0);
+const gradient2 = chart2.createLinearGradient(500, 0, 100, 0);
 
-const salesChart = document.querySelector("#salesChart").getContext("2d");
+gradient1.addColorStop(0, "#991bfa");
+gradient1.addColorStop(0, "#e323ff");
+gradient2.addColorStop(0, "#f17401");
+gradient2.addColorStop(0, "#ffb524");
 
-const presentationChart = document
-  .querySelector("#presentationChart")
-  .getContext("2d");
-const developmentChart = document
-  .querySelector("#developmentChart")
-  .getContext("2d");
-const researchChart = document.querySelector("#researchChart").getContext("2d");
+// const chart3 = createChart("salesChart");
+const chart4 = createChart("developmentChart");
+const chart5 = createChart("researchChart");
+const chart6 = createChart("presentationChart");
 
 // Charts Options
-
+/*EARNING BARS OPTIONS */
 const barChartOptions = {
   type: "bar",
 
@@ -58,21 +60,21 @@ const barChartOptions = {
     },
   },
 };
-
+/*EARNING LINES OPTIONS */
 const lineChartOptions = {
   type: "line",
 
   data: {
     datasets: [
       {
-        borderColor: gradientStroke,
+        borderColor: gradient1,
         backgroundColor: "rgba(0, 0, 0,0)",
         data: [12, 10, 5, 2, 20, 30, 45, 50],
         pointRadius: 0,
         borderWidth: 4,
       },
       {
-        borderColor: gradientStroke,
+        borderColor: gradient2,
         backgroundColor: "rgba(0, 0, 0,0)",
         data: [20, 12, 5, 20, 50, 28, 45, 25],
         pointRadius: 0,
@@ -111,97 +113,124 @@ const lineChartOptions = {
     },
   },
 };
-
-const lineChart2Options = {
+/*PRESENTATION, DEVELOPMENT AND RESEARCH OPTIONS */
+const lineOptions = {
+  legend: false,
+  responsive: true,
+  maintainAspectRatio: false,
+  scales: {
+    yAxes: [
+      {
+        display: false,
+      },
+    ],
+    xAxes: [
+      {
+        display: true,
+        labels: ["", "", "", ""],
+      },
+    ],
+  },
+  tooltips: false,
+  layout: {
+    padding: {
+      top: 5,
+      left: 0,
+      right: 0,
+      bottom: 5,
+    },
+  },
+};
+const presentationOptions = {
   type: "line",
   data: {
     datasets: [
       {
-        borderColor: gradientStroke,
+        borderColor: "#991bfa",
         data: [2, 60, 10, 50],
         pointRadius: 0,
         borderWidth: 2,
       },
     ],
   },
-  options: {
-    legend: false,
-    responsive: true,
-    maintainAspectRatio: false,
-    scales: {
-      yAxes: [
-        {
-          display: false,
-        },
-      ],
-      xAxes: [
-        {
-          display: true,
-          labels: ["", "", "", ""],
-        },
-      ],
-    },
-    tooltips: false,
-    layout: {
-      padding: {
-        top: 5,
-        left: 0,
-        right: 0,
-        bottom: 5,
-      },
-    },
-  },
+  options: lineOptions,
 };
-
-const salesChartOptions = {
-  type: "doughnut",
+const developmentOptions = {
+  type: "line",
   data: {
     datasets: [
       {
-        data: [10, 15, 30],
-        backgroundColor: ["#7517F8", "#FF7D05", "#02C751"],
-        borderWidth: 0,
-        borderAlign: "inner",
+        borderColor: "#4dffdf",
+        data: [2, 60, 10, 50],
+        pointRadius: 0,
+        borderWidth: 2,
       },
     ],
-    labels: ["red", "blue", "green"],
   },
-  options: {
-    responsive: true,
-    legend: {
-      display: false,
-    },
-    title: {
-      display: false,
-      text: "Chart.js Doughnut Chart",
-    },
-    animation: {
-      animateScale: true,
-      animateRotate: true,
-    },
-    cutoutPercentage: 70,
-    layout: {
-      padding: {
-        top: 5,
-        left: 0,
-        right: 0,
-        bottom: 5,
-      },
-    },
-  },
+  options: lineOptions,
 };
+
+const researchOptions = {
+  type: "line",
+  data: {
+    datasets: [
+      {
+        borderColor: "#f17401",
+        data: [2, 60, 10, 50],
+        pointRadius: 0,
+        borderWidth: 2,
+      },
+    ],
+  },
+  options: lineOptions,
+};
+/* EARNINGS CIRCLE OPTIONS */
+// const salesChartOptions = {
+//   type: "doughnut",
+//   data: {
+//     datasets: [
+//       {
+//         data: [10, 15, 30],
+//         backgroundColor: ["#7517F8", "#FF7D05", "#02C751"],
+//         borderWidth: 0,
+//         borderAlign: "inner",
+//       },
+//     ],
+//     labels: ["red", "blue", "green"],
+//   },
+//   options: {
+//     responsive: true,
+//     legend: {
+//       display: false,
+//     },
+//     title: {
+//       display: false,
+//       text: "Chart.js Doughnut Chart",
+//     },
+//     animation: {
+//       animateScale: true,
+//       animateRotate: true,
+//     },
+//     cutoutPercentage: 70,
+//     layout: {
+//       padding: {
+//         top: 5,
+//         left: 0,
+//         right: 0,
+//         bottom: 5,
+//       },
+//     },
+//   },
+// };
 
 //Charts Inicialization
 
-const barChartInstance = new Chart(barChart, barChartOptions);
+const barChartInstance = new Chart(chart1, barChartOptions);
 
-const lineChartInstance = new Chart(lineChart, lineChartOptions);
+const lineChartInstance = new Chart(chart2, lineChartOptions);
 
-const presentationChartInstance = new Chart(
-  presentationChart,
-  lineChart2Options
-);
-const developmentChartInstance = new Chart(developmentChart, lineChart2Options);
-const researchChartChartInstance = new Chart(researchChart, lineChart2Options);
+const presentationChartInstance = new Chart(chart6, presentationOptions);
+const developmentChartInstance = new Chart(chart4, developmentOptions);
+const researchChartChartInstance = new Chart(chart5, researchOptions);
 
-const salesChartInstance = new Chart(salesChart, salesChartOptions);
+// const salesChartInstance = new Chart(chart3, salesChartOptions);
